e90c06847918623bef32a98be75e2294866430a8
chaliju <chaliju1997@163.com>
2021-08-26 13:53:52 +0800

    readbook项目后端接口的跟新

---
+++ const.js
@@ -0,0 +1,33 @@
+const resUrl = 'http://192.168.2.13:8081'
+const mp3FilePath = 'D:/1-upup/web/myself-note/Ebook-app/work-resource/mp3'
+
+const category = [
+  'Biomedicine',
+  'BusinessandManagement',
+  'ComputerScience',
+  'EarthSciences',
+  'Economics',
+  'Engineering',
+  'Education',
+  'Environment',
+  'Geography',
+  'History',
+  'Laws',
+  'LifeSciences',
+  'Literature',
+  'SocialSciences',
+  'MaterialsScience',
+  'Mathematics',
+  'MedicineAndPublicHealth',
+  'Philosophy',
+  'Physics',
+  'PoliticalScienceAndInternationalRelations',
+  'Psychology',
+  'Statistics'
+]
+
+module.exports = {
+  resUrl,
+  category,
+  mp3FilePath
+}
